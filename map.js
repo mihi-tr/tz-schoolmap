fe41b0414f072cc929c7740737dff16f583610b0
Michael Bauer <mihi@lo-res.org>
2014-10-04 21:17:22 +0200

    simplify map - create a single layer with two sublayers
    This reduces the requests made to CartoDB - however it changes the behavior
    of the map slightly

--- map.js
+++ map.js
@@ -26,34 +26,28 @@ window.onload = function() {
      sql.getBounds("SELECT * FROM districts_1").done(function(b) {
         map.fitBounds(b) })
      L.tileLayer("http://{s}.api.cartocdn.com/base-light/{z}/{x}/{y}.png").addTo(map);
-     var sh=cartodb.createLayer(map, {
-            user_name: 'svasdev',
-            type: 'cartodb',
-            sublayers: [{
-                sql: "SELECT * FROM districts_1",
-                cartocss: '#districts_1 {  polygon-fill: #FF6600;'+
-                              ' polygon-opacity: 0.0; '+
-                              ' line-color: #ada7a7;'+
-                              ' line-width: 1;'+
-                              ' line-opacity: .8;'+
-                              ' line-dasharray: 2; }'
-    }]
-    })
-    .addTo(map);
     
     cartodb.createLayer(map, {
                 user_name: 'svasdev',
                 type: 'cartodb',
                 sublayers: [{
+                    sql: "SELECT * FROM districts_1",
+                    cartocss: '#districts_1 {  polygon-fill: #FF6600;'+
+                                  ' polygon-opacity: 0.0; '+
+                                  ' line-color: #ada7a7;'+
+                                  ' line-width: 1;'+
+                                 ' line-opacity: .8;'+
+                                 ' line-dasharray: 2; }' },
+                    {             
                     sql: "SELECT * FROM schools_merged_2013_correct_ratio_only",
                     interactivity: interactivity.join(","),
-                    cartocss: $("#schoolcss").html(),
+                    cartocss: $("#schoolcss").html() 
     }]
     })
     .addTo(map)
     .on('done', function(l) {
         console.log('done');
-        l.getSubLayer(0).setInteraction(true);
+        l.getSubLayer(1).setInteraction(true);
         l.on('featureClick',function(e,ll, p, data, l) {
             console.log(data);
             $("#info").html(Mustache.render($("#infowindow").html(), data));
@@ -68,7 +62,7 @@ window.onload = function() {
     
     var createSchools = function(map,query) {
         query = "SELECT * FROM schools_merged_2013_correct_ratio_only " + (query || "")
-        layer.getSubLayer(0).setSQL(query); }
+        layer.getSubLayer(1).setSQL(query); }
 
    //createSchools(map,"");     
     
